98c4fd8bc7d8ff80bb62f0ebebb6972b820ac6b4
Blake Beaupain <me@blake.sh>
2022-07-26 12:40:51 -0700

    update host and port

--- src/main/java/org/example/MicrohttpTest.java
+++ src/main/java/org/example/MicrohttpTest.java
@@ -3,6 +3,7 @@ package org.example;
 import org.microhttp.EventLoop;
 import org.microhttp.Handler;
 import org.microhttp.Header;
+import org.microhttp.Options;
 import org.microhttp.Response;
 
 import java.util.List;
@@ -15,7 +16,10 @@ public class MicrohttpTest {
             List.of(new Header("Content-Type", "text/plain")),
             "hello world\n".getBytes());
         Handler handler = (req, callback) -> callback.accept(response);
-        EventLoop eventLoop = new EventLoop(handler);
+        Options options = new Options()
+            .withHost("0.0.0.0")
+            .withPort(8080);
+        EventLoop eventLoop = new EventLoop(options, handler);
         eventLoop.start();
         eventLoop.join();
     }
